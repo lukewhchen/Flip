800f8b445c33f4ba33ff36541097d995c73ce379
lukewhchen <lukewhchen@gmail.com>
2018-05-31 12:09:32 -0400

    add optional mobile width

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ $(() => {
 	let arr = [ [false, false], [false, false] ];
 	let length = arr.length;
 	if ($(window).width() < 750) {
-		$(".container").css("max-width", "50%");
+		$(".container").css("max-width", "80%");
 	}
 	let dWidth = $(".container").width();
 
