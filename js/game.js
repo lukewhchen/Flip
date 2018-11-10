9d3b2514bf845d99f342652301518d6aea1b7610
lukewhchen <lukewhchen@gmail.com>
2018-11-09 16:40:16 -0800

    add toggle class

--- js/game.js
+++ js/game.js
@@ -113,7 +113,7 @@ $(() => {
 	// });
 
 	$('i.button').click(function(){
-		$(this).toggleClass('fa-volume-up fa-volume-off');
+		$(this).toggleClass("fa-volume-up fa-volume-off");
 		if (music === true) {
 			music = false;
 		}else {
