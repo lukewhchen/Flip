da5d3f0f8de6db4b7ec27fdc8922e36fbdf9c117
lukewhchen <lukewhchen@gmail.com>
2019-09-18 15:40:01 -0700

    refactoring

--- js/game.js
+++ js/game.js
@@ -1,15 +1,14 @@
 $(() => {
+	if ($(window).width() < 750) $(".container").css("max-width", "100%");
 	let arr = [ [false, false], [false, false] ];
 	let length = arr.length;
 	let audio = $("#mysoundclip")[0];
 	let audio1 = $("#mysoundclip1")[0];
 	let music = true;
-	if ($(window).width() < 750) {
-		$(".container").css("max-width", "100%");
-	}
 	let dWidth = $(".container").width();
+	renderBoard();
 
-	function setBoard() {
+	function renderBoard() {
     let board = $(".board");
 		board.empty();
 
@@ -21,90 +20,70 @@ $(() => {
 				let width = (dWidth - 30 - (length * 2)) / length;
 				let square = ('<div class="square" ></div>');
 				board.children(".row").eq(i).append(square)
-					.find(".square").eq(j).attr("pos", i + "," + j).css({"width": width, "height": width})
+					.find(".square").eq(j).attr("pos", i + "," + j)
+					.css({"width": width, "height": width})
 					.addClass(() => {
-						if (!arr[i][j]) {
-							return "unselected";}
+						if (!arr[i][j]) return "unselected";
 					});
 			}
 		}
-
+		
 		$(".square").click(function() {
-			if (music === true) {
-				audio.play();
-			}
+			if (music) audio.play();
 			let position = $(this).attr("pos").split(",");
 			let x = Number(position[0]);
 			let y = Number(position[1]);
-
-			arr[x][y] = !arr[x][y];
-  			if ((x - 1) >= 0) {
-  					arr[x - 1][y] = !arr[x - 1][y];
-  			}
-  			if ((x + 1) < length) {
-  					arr[x + 1][y] = !arr[x + 1][y];
-  			}
-  			if ((y - 1) >= 0) {
-  					arr[x][y - 1] = !arr[x][y - 1];
-  			}
-  			if ((y + 1) < length) {
-  					arr[x][y + 1] = !arr[x][y + 1];
-  			}
-
-			setBoard(arr);
+			handleClick(x,y);
 		});
-
-		const checkBoard = () => {
-			for (let i = 0; i < length; i++) {
-				for (let j = 0; j < length; j++) {
-					if (!arr[i][j]) {
-						return false;
-					}
-				}
-			}
-			if (music === true) {
-				audio1.play();
-			}
-			$("#level-modal").show();
-
-			let newArr = [];
-			for (let i = 0; i < (length + 1); i++) {
-				newArr[i] = [];
-				for (let j = 0; j < (length + 1); j++) {
-					newArr[i][j] = false;
-				}
-			}
-
-			arr = newArr;
-			length = arr.length;
-			setBoard(arr);
-		};
-
-		setTimeout(checkBoard,300);
 	}
 
-	setBoard(arr);
+	const checkBoard = () => {
+		for ( let i = 0; i < length; i++) {
+			if ( arr[i].includes(false)) return;
+		}
+		if (music) audio1.play();
+		$("#level-modal").show();
+		setBoard(length+1);
+	};
 
-	$(".restart").click(() => {
-		arr = [ [false, false], [false, false] ];
-    length = arr.length;
-		setBoard(arr);
-	});
-
-	$(".retry").click(() => {
-		arr = [];
-		for (let i = 0; i < length; i++) {
-			arr[i] = [];
-			for (let j = 0; j < length; j++) {
-				arr[i][j] = false;
+	const setBoard = (len) => {
+		let newArr = [];
+		for (let i = 0; i < (len); i++) {
+			newArr[i] = [];
+			for (let j = 0; j < (len); j++) {
+				newArr[i][j] = false;
 			}
 		}
-		setBoard(arr);
-	});
+		arr = newArr;
+		length = arr.length;
+		renderBoard();
+	};
 
-	$('i.button').click(function(){
-		$(this).toggleClass("fa-volume-up fa-volume-off");
-		if (music === true) {
+	const handleClick = (x,y) => {
+		arr[x][y] = !arr[x][y];
+		if ((x - 1) >= 0) {
+				arr[x - 1][y] = !arr[x - 1][y];
+		}
+		if ((x + 1) < length) {
+				arr[x + 1][y] = !arr[x + 1][y];
+		}
+		if ((y - 1) >= 0) {
+				arr[x][y - 1] = !arr[x][y - 1];
+		}
+		if ((y + 1) < length) {
+				arr[x][y + 1] = !arr[x][y + 1];
+		}
+		renderBoard();
+		setTimeout(() => checkBoard(), 1000);
+	};
+
+
+	$(".restart").click(() => setBoard(2));
+	$(".retry").click(() => setBoard(length));
+
+	$('i.button').click(() => {
+		$('i.button').toggleClass("fa-volume-up fa-volume-off");
+		if (music) {
 			music = false;
 			$('span').html("Unmute");
 		}else {
@@ -113,12 +92,7 @@ $(() => {
 		}
 	});
 
-	$(".instructions").click(() => {
-		$("#rule-modal").show();
-	});
-
-	$(".play-button").click(() => {
-		$(".modal").hide();
-	});
+	$(".instructions").click(() => $("#rule-modal").show());
+	$(".play-button").click(() => $(".modal").hide());
 
 });
